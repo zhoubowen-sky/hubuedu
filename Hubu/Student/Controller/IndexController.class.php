39f6616fe11c2dabed701e91f3f9d33f7a60cc34
周博文 <zhoubowen.sky@gmail.com>
2016-12-21 00:33:48 +0800

    项目文件目录更改
    改为单一入口index.php 项目分组，三个模块

---
+++ Hubu/Student/Controller/IndexController.class.php
@@ -0,0 +1,12 @@
+<?php
+// 后台管理首页的控制器
+namespace Student\Controller;
+use Think\Controller;
+class IndexController extends Controller {
+	
+    function index(){
+		echo "这里是学生模块，以后是网站首页" ;
+	}
+	
+	
+}
