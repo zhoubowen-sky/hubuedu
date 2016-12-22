3317ceceb7cd75cb4b3d7514ee0aa0ffd9fbd5c7
周博文 <zhoubowen.sky@gmail.com>
2016-12-22 13:18:12 +0800

    admin index方法添加session判断
    只有当有用户登陆的时候才可以访问后台管理页面，否则直接跳转到login页面

--- Hubu/Admin/Controller/AdminIndexController.class.php
+++ Hubu/Admin/Controller/AdminIndexController.class.php
@@ -7,6 +7,14 @@ class AdminIndexController extends Controller {
      * 用以展示后台管理首页的模板
      */
     function index(){
+        
+        //用session判断当前是否有用户登录，如果没有登录则不允许访问index
+    	//且跳转到login页面
+        if(!session()){
+            //如果session为空跳转到login
+            $this->redirect('AdminUser/login');
+    	}
+    	
 		//下面是用来输出所有常量的语句
 		//var_dump(get_defined_constants(true));
         //调用视图display()，直接展示index.html模板，display()没有参数那么调用的模板名称就与当前方法名一致
