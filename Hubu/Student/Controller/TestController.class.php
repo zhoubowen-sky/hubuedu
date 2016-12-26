49e6c73d329b5fb03452903d442c535f347df85f
周博文 <zhoubowen.sky@gmail.com>
2016-12-26 19:55:46 +0800

    自动发送邮件功能测试完成

--- Hubu/Student/Controller/TestController.class.php
+++ Hubu/Student/Controller/TestController.class.php
@@ -41,5 +41,41 @@ class TestController extends Controller {
 	    show_bug($bbb);
 	}
 	
+	function emailCheck(){
+	    $a = think_send_mail('zhoubowen.sky@foxmail.com','发件人名称填写处','湖大在线学习平台','这是一封测试邮件，无需回复！不要太在意为什么是126邮箱。。。这并不重要。。。');
+	    show_bug($a);
+	    //think_send_mail('要发送的邮箱','发送人名称，即你的名称','邮件主题','邮件内容');
+	    
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
