739b79d84ac164c904456d2155f1a6de1bbbf971
周博文 <zhoubowen.sky@gmail.com>
2016-12-31 17:36:36 +0800

    Student登陆以及邮件验证功能完成
    还没有生成Student的session，邮件验证，校验码都完成了，如果账号没有激活过，下次登陆的时候会给出提示并自动发送激活邮件到注册邮箱

--- Hubu/Model/StudentUserModel.class.php
+++ Hubu/Model/StudentUserModel.class.php
@@ -5,5 +5,58 @@ use Think\Model;
 
 //父类是Model 
 class StudentUserModel extends Model{
+    //可以自定义数据表的相关属性，例如数据表前缀，名称等等，进行个性化的设置
+    
+    
+    /**
+     * 功能：验证用户名以及密码  供控制器使用
+     * @param 登陆表单中用户输入的用户名/邮箱 $name
+     * @param 登陆表单中用户输入的密码 $pwd
+     * @return boolean|unknown
+     */
+    function checkNamePwd($name,$pwd){
+        //根据$name查询数据库里面是否有这一条记录
+        //可以根据指定字段进行查询getByXXX($name)，XXX是字段名，Model父类封装好的方法，执行结果一位数组，PHP本身不区分大小写，采用驼峰法命名
+        //此处驼峰法命名与数据库之间有一个下划线_的关系,AdminUser对应数据表名字hubu_admin_user,hubu_为数据表前缀
+        $info = $this-> getBystudent_user_email($name);
+        //show_bug($info);//$info是一个以为数组才是正确，为空说明表单用户名输入错误
+        //$info不为空，继续验证密码
+        if($info != null){
+            //验证密码,查询出来儿密码对比用户输入的密码
+            if($info['student_user_pwd'] != $pwd){
+                return false;//返回false
+            } else {
+                //密码输入正确
+                return $info;//返回$info，回头生成session信息
+            }
+        } else {
+            //密码错误
+            return false;//返回false
+        }
+    }
+    
+    
+    
+    /**
+     * 更新学生用户的账户激活状态
+     * @param 传入的id的值 $student_user_id
+     */
+    function activeAccount_Model($student_user_id=0){
+        header("Content-Type:text/html; charset=utf-8");//设置后不乱码
+        
+        //直接使用SQL语句来更新数据，建议少使用SQL语句，容易加大SQL注入的风险，尽量用框架提供的方法来做
+        //$sql = "UPDATE hubu_student_user SET student_user_verify = 1 WHERE student_user_id = $student_user_id";
+        //$a = $this->execute($sql);
+        
+        $User = M("StudentUser"); // 实例化对象
+        $b = $User-> where("student_user_id = $student_user_id")->setField('student_user_verify',1);
+        //show_bug($b);
+        return $b;
+    }
+    
+    
+    
+    
+    
     
 }
--- Hubu/Student/Controller/StudentUserController.class.php
+++ Hubu/Student/Controller/StudentUserController.class.php
@@ -10,18 +10,114 @@ class StudentUserController extends Controller {
 	 * 并对其进行校验，生成session
 	 */
 	function login(){
-	    $z = D('StudentUser');
-	    //show_bug($z);
-	    $a = $z->select();
-	    show_bug($a);
-	    $this->display();
+	    //判断用户是否提交了表单
+	    if(!empty($_POST)){
+	        //提交了表单
+	        //print_r($_POST);
+	        //验证码校验，实例化一个Verify对象,调用其中的check方法校验验证码
+	        $verify = new \Think\Verify();
+	        if(!$verify ->check($_POST['code'])){
+	            //echo "验证码错误！";
+	            $this->error("验证码输入错误！");
+	        } else {
+	            //验证码输入正确，账号密码校验
+	            $user = new \Model\StudentUserModel();//实例化StudentUserModel调用其中的checkNamePwd()方法,好对student_user数据库中的数据进行操作
+	            $z = $user->checkNamePwd($_POST['student_user_email'], $_POST['student_user_pwd']);
+	            //show_bug($z);
+	            //判断账号密码是否验证成功
+	            if($z === false){
+	                //账号密码输入错误
+	                $this->error("输入的用户名或密码错误!");
+	            }else {
+	                //账号密码输入正确
+	                //进行账号激活校验，即student_user_verify字段，只有此字段为真（1）才能进行下一步操作，否则发送邮件激活
+	                $rst = $user->getField('student_user_verify');
+	                //show_bug($rst);
+	                if (!$rst){
+	                    //邮箱没有验证,提示发送邮件验证
+	                    $email = $user->getField('student_user_email');//获取邮箱字段的值
+	                    $id = $user->getField('student_user_id');//获取id
+	                    //发送激活邮件
+	                    //show_bug($email);
+	                    //账号激活的URL地址,例如  http://web.hubu.edu:3002/index.php/Student/StudentUser/activeAccount/id/1
+	                    $activeURL = SITE_URL.'index.php/Student/StudentUser/activeAccount/id/'.$id.' ';
+	                    //show_bug($activeURL);
+	                    $a = think_send_mail($email,'湖北大学网络课程平台','账号激活邮件','您的账号为：'.$email.',点击链接激活账号：'.$activeURL);
+	                    $this->error("您的账号还没有激活，我们已向您的邮箱 $email 发送了激活邮件，请注意查收！");
+	                }else {
+	                    /*****登陆成功后执行的操作****/
+	                    echo "登陆成功";
+	                    
+	                }
+	            }
+	        }
+	    }
+	}
+	
+	/**
+	 * 用以激活学生用户的账号
+	 * 用户点击这个链接后实现对数据库中student_user_verify字段的更改，将其值改为1
+	 * @param 要传入的学生用户那一天记录的id值 $id
+	 */
+	function activeAccount($id=0){
+	    if($id){
+	        //echo $id;
+	        //传入的id值大于0，即不是默认值，对student_user数据表中字段进行修改
+	        $user = new \Model\StudentUserModel();//实例化StudentUser,调用其中的方法来操作数据库，注意，数据库的操作尽量都留在Model里面
+	        $z = $user->activeAccount_Model($id);
+	        //show_bug($z);
+	        if($z){
+	            //echo "^_^恭喜你，账号激活成功！点击链接访问：".SITE_URL;
+	            $this->success('^_^恭喜你，账号激活成功！',SITE_URL);
+	        }
+	        
+	    }else {
+	        //echo "额。。。貌似出现了什么了不得的东西，账号激活失败，请联系网站管理员！";
+	        $this->error('额，账号激活失败。貌似出现了什么了不得的东西，请联系网站管理员！',SITE_URL);
+	    }
+	    
 	}
 	
 	/**
 	 * 用户个人中心
+	 * 展示个人基本信息，提供基本信息的修改功能
 	 */
 	function personalCenter(){
 	    $this->display();
+	}
+	
+	/**
+	 * 学生用户注册的方法
+	 * 接收注册时候的表单，并向数据库写入注册用户信息
+	 */
+	function register(){
+	    
+	}
+	
+	/**
+	 * 名称：verifyImg()
+	 * 功能：实现验证发的生成
+	 */
+	function verifyImg(){
+	    //配置验证码的数组
+	    $config = array(
+	        'expire'    =>  1800,            // 验证码过期时间（s）
+	        'useZh'     =>  false,           // 使用中文验证码
+	        'useImgBg'  =>  false,           // 使用背景图片
+	        'fontSize'  =>  14,              // 验证码字体大小(px)
+	        'useCurve'  =>  false,            // 是否画混淆曲线
+	        'useNoise'  =>  false,            // 是否添加杂点
+	        'imageH'    =>  32,               // 验证码图片高度
+	        'imageW'    =>  100,               // 验证码图片宽度
+	        'length'    =>  4,               // 验证码位数
+	        'fontttf'   =>  '',              // 验证码字体，不设置随机获取
+	        'bg'        =>  array(243, 251, 254),  // 背景颜色
+	        'reset'     =>  true,           // 验证成功后是否重置
+	    );
+	    //实例化的时候传入配置
+	    $verify = new \Think\Verify($config); //实例化Verify对象，此处要注意命名空间的书写，可以使用在admin.php中定义的show_bug()函数输出对象看实例化好了没有
+	    //show_bug($verify);
+	    $verify ->entry();
 	}
     
     //下面的方法作为书写login的参考
