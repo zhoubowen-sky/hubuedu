994c0263e287156d69d5eb60e794ccc3a8840895
童天相 <wasdwasdk88@qq.com>
2017-01-03 16:46:03 +0800

    弹出层完善
    BUG不知道怎么消失了~~~~

--- Hubu/Public/js/layer.js
+++ Hubu/Public/js/layer.js
@@ -1,12 +1,22 @@
 ﻿$(document).ready(function(){
-  $('#login-div-1').css("display", "block");
+  $('#login-div-1').hide();
+  $('#login-div-2').hide();
 });
  $('#login').on('click', function(){
         layer.open({
-            type: 2,
-            title: ['登录', 'font-size:18px;background: #1e6f46;color: white;padding-left:160px;'],
-            area: ['400px', '300px'],
+            type: 1,
+            title: ['登录', 'font-size:18px;background: #1e6f46;color: white;padding-left:200px;'],
+            area: ['460px', '350px'],
             shadeClose: true, //点击遮罩关闭
-			content: '$("#login-div-1")'
+			content: $('#login-div-1')
+        });
+    });
+ $('#login2').on('click', function(){
+        layer.open({
+            type: 1,
+            title: ['注册', 'font-size:18px;background: #1e6f46;color: white;padding-left:200px;'],
+            area: ['460px', '350px'],
+            shadeClose: true, //点击遮罩关闭
+            content: $('#login-div-2')
         });
     });
