58bea6a95db64b34084db94c34302e3b1c8df3a5
周博文 <zhoubowen.sky@gmail.com>
2017-01-03 20:18:17 +0800

    修改MySQL数据库为远程数据库
    数据库IP地址120.27.104.19

--- Hubu/Common/Conf/config.php
+++ Hubu/Common/Conf/config.php
@@ -30,7 +30,7 @@ return array(
     
     /* 数据库连接相关的配置 */
     'DB_TYPE'               =>  'mysql',     // 数据库类型
-    'DB_HOST'               =>  'localhost', // 服务器地址
+    'DB_HOST'               =>  '120.27.104.19', // 服务器地址
     'DB_NAME'               =>  'hubuedu_data',          // 数据库名
     'DB_USER'               =>  'root',      // 用户名
     'DB_PWD'                =>  '',          // 密码
