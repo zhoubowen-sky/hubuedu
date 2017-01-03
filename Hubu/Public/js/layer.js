c5d663bf435cea6bc81fb6b109d9c2f491059155
童天相 <wasdwasdk88@qq.com>
2017-01-03 17:03:42 +0800

    小微调

--- Hubu/Public/js/layer.js
+++ Hubu/Public/js/layer.js
@@ -5,8 +5,8 @@
  $('#login').on('click', function(){
         layer.open({
             type: 1,
-            title: ['登录', 'font-size:18px;background: #1e6f46;color: white;padding-left:200px;'],
-            area: ['460px', '305px'],
+            title: ['登录', 'font-size:18px;background: #1e6f46;color: white;padding-left:185px;'],
+            area: ['420px', '305px'],
             shadeClose: true, //点击遮罩关闭
 			content: $('#login-div-1')
         });
@@ -14,8 +14,8 @@
  $('#login2').on('click', function(){
         layer.open({
             type: 1,
-            title: ['注册', 'font-size:18px;background: #1e6f46;color: white;padding-left:200px;'],
-            area: ['460px', '360px'],
+            title: ['注册', 'font-size:18px;background: #1e6f46;color: white;padding-left:185px;'],
+            area: ['420px', '360px'],
             shadeClose: true, //点击遮罩关闭
             content: $('#login-div-2')
         });
