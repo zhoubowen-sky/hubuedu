e4e514d8b6f01a6f6b3997354794a1560262b258
童天相 <wasdwasdk88@qq.com>
2017-01-03 23:30:24 +0800

    修复BUG

--- Hubu/Public/js/layer.js
+++ Hubu/Public/js/layer.js
@@ -1,22 +1,32 @@
-﻿$(document).ready(function(){
-  $('#login-div-1').hide();
-  $('#login-div-2').hide();
-});
- $('#login').on('click', function(){
-        layer.open({
-            type: 1,
-            title: ['登录', 'font-size:18px;background: #1e6f46;color: white;padding-left:185px;'],
-            area: ['420px', '305px'],
-            shadeClose: true, //点击遮罩关闭
-			content: $('#login-div-1')
-        });
-    });
+﻿if($('.layui-layer'.length==0)){
+    $('#login-div-1').hide();
+    $('#login-div-2').hide();
+}
+ $('#login').on('click', function() {
+     layer.open({
+         type: 1,
+         title: ['登录', 'font-size:18px;background: #1e6f46;color: white;padding-left:185px;'],
+         area: ['420px', '305px'],
+         shadeClose: true, //点击遮罩关闭
+         end:function () {
+             $('#login-div-1').hide();
+         },
+         content: $('#login-div-1')
+     });
+     
+
+ });
  $('#login2').on('click', function(){
         layer.open({
             type: 1,
             title: ['注册', 'font-size:18px;background: #1e6f46;color: white;padding-left:185px;'],
             area: ['420px', '360px'],
             shadeClose: true, //点击遮罩关闭
+            end:function () {
+                $('#login-div-2').hide();
+            },
             content: $('#login-div-2')
         });
-    });
+});
+
+    
