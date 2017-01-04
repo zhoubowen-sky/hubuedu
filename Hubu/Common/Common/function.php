5052101fc8bbd36612748fe592e8bee366711529
周博文 <zhoubowen.sky@gmail.com>
2017-01-04 22:57:47 +0800

    公共函数库中添加getPage方法，对TP自带Page的封装

--- Hubu/Common/Common/function.php
+++ Hubu/Common/Common/function.php
@@ -41,3 +41,21 @@ function think_send_mail($to, $name, $subject = '', $body = '', $attachment = nu
     return $mail->Send() ? true : $mail->ErrorInfo;
 
 }
+
+/**
+ * TODO 基础分页Page的相同代码封装
+ * @param $count 要分页的总记录数
+ * @param int $pagesize 每页查询条数
+ * @return \Think\Page
+ */
+function getPage($count, $pagesize = 10){
+    $page=new Think\Page($count,$pagesize);
+    $page->setConfig('header', '<li>共<b>%TOTAL_ROW%</b>条记录 <b>%NOW_PAGE%</b>/<b>%TOTAL_PAGE%</b>页</li>');
+    $page->setConfig('prev', '上一页');
+    $page->setConfig('next', '下一页');
+    $page->setConfig('last', '末页');
+    $page->setConfig('first', '首页');
+    $page->setConfig('theme', '%FIRST%%UP_PAGE%%LINK_PAGE%%DOWN_PAGE%%END%%HEADER%');
+    $page->lastSuffix=false;//最后一页不显示总页数
+    return $page;
+}
