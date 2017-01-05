686ea3bb191071a602a7466a721cc074ec2828ec
周博文 <zhoubowen.sky@gmail.com>
2017-01-05 12:22:17 +0800

    更新getPage公共方法

--- Hubu/Common/Common/function.php
+++ Hubu/Common/Common/function.php
@@ -42,20 +42,30 @@ function think_send_mail($to, $name, $subject = '', $body = '', $attachment = nu
 
 }
 
+
 /**
- * TODO 基础分页Page的相同代码封装
- * @param $count 要分页的总记录数
+ * TODO 基础分页的相同代码封装，使前台的代码更少
+ * @param $m 模型，引用传递
+ * @param $where 查询条件
  * @param int $pagesize 每页查询条数
  * @return \Think\Page
  */
-function getPage($count, $pagesize = 10){
-    $page=new Think\Page($count,$pagesize);
-    $page->setConfig('header', '<li>共<b>%TOTAL_ROW%</b>条记录 <b>%NOW_PAGE%</b>/<b>%TOTAL_PAGE%</b>页</li>');
-    $page->setConfig('prev', '上一页');
-    $page->setConfig('next', '下一页');
-    $page->setConfig('last', '末页');
-    $page->setConfig('first', '首页');
-    $page->setConfig('theme', '%FIRST%%UP_PAGE%%LINK_PAGE%%DOWN_PAGE%%END%%HEADER%');
-    $page->lastSuffix=false;//最后一页不显示总页数
-    return $page;
+function getPage(&$m,$where,$pagesize=10){
+    $m1=clone $m;//浅复制一个模型
+    $count = $m->where($where)->count();//连惯操作后会对join等操作进行重置
+    $m=$m1;//为保持在为定的连惯操作，浅复制一个模型
+    $p=new Think\Page($count,$pagesize);
+    $p->lastSuffix=false;
+    $p->setConfig('header','<li class="rows">共<b>%TOTAL_ROW%</b>条记录&nbsp;&nbsp;第<b>%NOW_PAGE%</b>页/共<b>%TOTAL_PAGE%</b>页</li>');
+    $p->setConfig('prev','上一页');
+    $p->setConfig('next','下一页');
+    $p->setConfig('last','末页');
+    $p->setConfig('first','首页');
+    $p->setConfig('theme','%FIRST% %UP_PAGE% %LINK_PAGE% %DOWN_PAGE% %END% %HEADER%');
+
+    $p->parameter=I('get.');
+
+    $m->limit($p->firstRow,$p->listRows);
+
+    return $p;
 }
