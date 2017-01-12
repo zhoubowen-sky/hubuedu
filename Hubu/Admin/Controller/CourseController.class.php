62352c0a3c37abaf253101ef9f2bf8410f672c58
周博文 <zhoubowen.sky@gmail.com>
2017-01-12 22:17:59 +0800

    courseList模板添加switch判断标签，课程类别数字转文字

--- Hubu/Admin/Controller/CourseController.class.php
+++ Hubu/Admin/Controller/CourseController.class.php
@@ -48,35 +48,23 @@ class CourseController extends Controller {
             $this->display();
         }
     }
+    
+    function showCourseChapter(){
+        
+    }
 	
-    function feedback($feedback_id = 0){
-        //echo $feedback_id;
-        //获取“删除”按钮传回来的id，根据id来执行删除
-        if($feedback_id > 0){
-            //执行删除操作
-            $del = D('Feedback');
-            $rst = $del->delete($feedback_id);
-            //show_bug($rst);
-        }
+    function updateCourseInfo(){
         
-        //用户留言的浏览与管理页面
-		//echo "feedback";
-		//var_dump(get)
-		//从数据库中查询出数据，并输出到前台模板
-        $m = M('Feedback');
-        $where = '';
-        $p = getpage($m,$where,6);
-        $list = $m->field(true)->where($where)->select();
-        $this->list = $list;
-        $this->page = $p->show();
-        $this->assign('info',$list);
-        
-		//$feedback = D('Feedback');
-		//$info = $feedback->select();//从数据库中查询出数据存储在$info中
-		//show_bug($info);
-		//$this->assign('info',$info);
-		$this->display();
-	}
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
 	
 	
 	
