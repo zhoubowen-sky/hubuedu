362819ff7e0ca6b74d90f06c73e7f1b0ef1067fb
周博文 <zhoubowen.sky@gmail.com>
2017-01-26 20:40:16 +0800

    学生用户 个人中心 个人信息修改功能添加完成

--- Hubu/Student/Controller/IController.class.php
+++ Hubu/Student/Controller/IController.class.php
@@ -11,6 +11,12 @@ class IController extends Controller {
             header("Content-Type:text/html; charset=utf-8");//不乱码
             $student_user_id = session('student_user_id');
             //show_bug($student_user_id);
+            
+            /**从数据库中查询出用户的个人信息*/
+            $studentUserInfo = D('StudentUser')->where("student_user_id = $student_user_id")->find();
+            //$studentUserInfo['student_user_sex'] = (int)$studentUserInfo['student_user_sex'];//强制转换为int类型
+            //show_bug($studentUserInfo);
+            
             $myCourseID = M('ChooseCourse')->where("choose_course_student = $student_user_id")->select();//查询出所选的课程的ID已经评分等信息
             //show_bug($myCourseID);
             $myCourseInfo = array();//存储从数据库中查询到的已选课程的信息
@@ -23,7 +29,7 @@ class IController extends Controller {
             
             //show_bug($myCourseInfo);
             $this->assign('myCourseInfo',$myCourseInfo);
-            
+            $this->assign('studentUserInfo',$studentUserInfo);//向叶面输出用户信息
             $this->display();
         }else {
             $this->error('请先登录！',SITE_URL);
@@ -48,6 +54,10 @@ class IController extends Controller {
         }
     }
     
+    /**
+     * 给课程评分的方法
+     * @param 课程的ID信息 $course_name_id
+     */
     function marking($course_name_id = 0){
         if ($course_name_id){
             if (!empty($_POST)){
@@ -66,5 +76,89 @@ class IController extends Controller {
             $this->error('出现未知错误，请联系管理员',SITE_URL);
         }
     }
+    
+    /**
+     * 更改用户的密码
+     */
+    function updatePassword(){
+        //show_bug($_POST);
+        if (!empty($_POST)){
+            //echo "提交了表单";
+            $info = new \Model\StudentUserModel();
+            $rst = $info->checkNamePwd(session('student_user_username'), $_POST['mpass']);//从session中获取当前用户的用户名
+            //show_bug($rst);
+            if ($rst){
+                //存储表单接收到的数据
+                $arr = array(
+                    'student_user_pwd' => $_POST['renewpass'],/* 获取表单中的新密码 */
+                );
+                $student_user_id = session('student_user_id');//存储用户的ID信息
+                $resault = D('StudentUser')->where("student_user_id = $student_user_id")->save($arr);
+                //show_bug($resault);
+                if ($resault){
+                    $this->success('密码修改成功');
+                }else {
+                    $this->error('密码修改失败');
+                }
+            }else {
+                $this->error('密码输入错误');
+            }
+            
+        }else {
+            $this->error('没有提交表单');
+        }
+    }
+    
+    /**
+     * 更新用户的个人信息
+     */
+    function updateUserInfo(){
+        //show_bug($_POST);
+        if (!empty($_FILES['student_user_pic']['tmp_name'])){
+                //选择了头像图片
+                //自定义文件接收相关配置
+                $config = array(
+                    'rootPath'      =>  'Hubu/Public/', //保存根路径,Andim目录下面public目录定义为Admin的根目录，这里的路径设置是以admin.php所在路径为依据设置
+                    'savePath'      =>  'Uploads/', //保存路径为Uploads，TP框架会自动生成如2016-12-18的日期文件夹
+                );
+                //print_r($_FILES);//是个二维数组
+                $upload = new \Think\Upload($config);//实例化Upload对象
+                //show_bug($upload);
+                $z = $upload->uploadOne($_FILES['student_user_pic']);//执行上传操作
+                //print_r($z);
+                //show_bug($z);
+                if (!$z){
+                    //show_bug($upload->getError());
+                    //$this->error($upload->getError());//输出错误
+                    $this->error('头像上传失败');
+                } else {
+                    //$this->success("头像上传成功！");
+                    echo "头像上传成功！";
+                }
+                $_POST['student_user_pic'] = $z['savepath'].$z['savename'];/**这里存储用户头像的文件路径*/
+                $_POST['student_user_time'] = date('Y-m-d H:i:s');
+                $student_user_id = session('student_user_id');//存储用户的ID信息
+                $studentUserInfo = D('StudentUser')->create();
+                //show_bug($studentUserInfo);
+                //show_bug($_POST);
+                $rst = D('StudentUser')->where("student_user_id = $student_user_id")->save();//更新数据库
+                if ($rst){
+                    $this->success('数据更新成功！');
+                }else {
+                    //show_bug($rst);
+                    $this->error('数据更新失败！');
+                }
+        }else {
+            $this->error('没有选择头像文件');
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
     
 }
