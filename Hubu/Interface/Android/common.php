7c29a22325b4dd187bae85514b4318d3cd2af711
周博文 <zhoubowen.sky@gmail.com>
2017-02-11 18:04:15 +0800

    common添加根据hot_course数据表里面的ID信息查询出该课程的所有信息的方法

--- Hubu/Interface/Android/common.php
+++ Hubu/Interface/Android/common.php
@@ -59,11 +59,48 @@ function getChapterCount($course_name_id = 0){
             return Response::show(403, '数据库链接失败');
         }
         
-        //从数据库中查询出这门课程的选课人数
+        //从数据库中查询出这门课程的小节总数
         $sql = 'select * from hubu_course_chapter where course_chapter_course_name = '.$course_name_id;
         $result = mysql_query($sql, $connect);//结果集
         return mysql_num_rows($result);
         
+    }else {
+        return 0;
+    }
+}
+
+/**
+ * 根据hot_course数据表里面的ID信息查询出该课程的所有信息
+ * @param number $course_id
+ */
+function getHotCourse($course_id = 0){
+    
+    if ($course_id){
+        //连接数据库
+        //接收异常，数据库连接失败
+        try {
+            $connect = Db::getInstance()->connect();//mysql连接资源
+        } catch (Exception $e){
+            // $e->getMessage();//获取错误信息，调试模式使用
+            return Response::show(403, '数据库链接失败');
+        }
+        
+        $hot_course_list_tmp = array();//存储热门课程的临时数组
+        
+        $sql = 'select * from hubu_course_name where course_name_id = '.$course_id;
+        $result = mysql_query($sql, $connect);//查询出结果集
+        //循环取出结果集中的数据
+        while($course_name = mysql_fetch_assoc($result)) {
+            //var_dump($course_name);
+            $hot_course_list_tmp[] = $course_name;
+        }
+        //var_dump($hot_course_list_tmp);
+        if ($hot_course_list_tmp){
+            return $hot_course_list_tmp['0'];//只返回一维数组
+        }else {
+            return 0;
+        }
+    
     }else {
         return 0;
     }
