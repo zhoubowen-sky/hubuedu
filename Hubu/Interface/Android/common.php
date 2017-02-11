10fe66d478865b571a774422e1b7eb0e94cd1c28
周博文 <zhoubowen.sky@gmail.com>
2017-02-12 00:17:05 +0800

    通过课程ID获取课程章节列表信息的接口完成

--- Hubu/Interface/Android/common.php
+++ Hubu/Interface/Android/common.php
@@ -105,3 +105,76 @@ function getHotCourse($course_id = 0){
         return 0;
     }
 }
+
+/**
+ * 获取一门课程的章节信息
+ * @param 课程的ID信息 $course_id
+ */
+function getCourseSectionChapterList($course_id){
+    if (is_numeric($course_id) && $course_id > 0){
+        //查询数据库
+        try {
+            $connect = Db::getInstance()->connect();//mysql连接资源
+        } catch (Exception $e){
+            // $e->getMessage();//获取错误信息，调试模式使用
+            return Response::show(403, '数据库链接失败');
+        }
+
+        $sql_section = 'select * from hubu_course_section WHERE course_section_course_name = '.$course_id.' order by course_section_section_num';//查询章信息的SQL语句
+        //echo $sql_section;
+        $result_section = mysql_query($sql_section, $connect);//查询出章的结果集
+
+        $section = array();//存储章信息的临时数组
+        $chapter = array();//存储节信息的临时数组
+        $section_chapter = array();//存储章节信息的临时数组
+
+        //循环取出章结果集中的数据
+        while($tmp = mysql_fetch_assoc($result_section)) {
+            //var_dump($tmp);
+            $section[] = $tmp['course_section_name'];//$section是一维数组，专门存储章信息
+        }
+
+        //var_dump($section);//exit();
+
+        foreach ($section as $k => $v){
+            
+            /**下面查询出节的信息并与章信息对号入座*/
+            $sql_chapter = 'select * from hubu_course_chapter where course_chapter_course_name = '.$course_id." and course_chapter_section = $k+1  order by course_chapter_section,course_chapter_name";
+            $result_chapter = mysql_query($sql_chapter, $connect);//查询出节的结果集
+            //var_dump($result_chapter);
+            //循环取出节结果集中的数据
+            while($tmp2 = mysql_fetch_assoc($result_chapter)) {
+                //var_dump($tmp2);
+                $chapter[] = $tmp2;
+            }
+            
+            $section_chapter[$k]['chapter_name'] = $v;//章信息
+            
+            //var_dump($chapter);
+            //exit();
+            
+            //下面代码为更改$chapter里面的字段名称
+            $chapter_tmp = array();
+            foreach ($chapter as $kk => $vv){
+                $chapter_tmp[$kk]['section_id'] = $vv['course_chapter_id'];
+                $chapter_tmp[$kk]['name']       = $vv['course_chapter_name'];
+                $chapter_tmp[$kk]['video_url']  = ADMIN_IMG_UPLOADS.$vv['course_chapter_video_url'];
+                $chapter_tmp[$kk]['ppt_url']    = ADMIN_IMG_UPLOADS.$vv['course_chapter_ppt_url'];
+            }
+            
+            $section_chapter[$k]['section'] = $chapter_tmp;//节信息
+            
+            $chapter = array();//清空数组，每将一整章所有小节的信息赋值完就必须清空一次
+            $chapter_tmp = array();
+        }
+
+        //var_dump($section_chapter);
+
+        return $section_chapter;
+
+    }else {
+        return 0;
+    }
+}
+
+
